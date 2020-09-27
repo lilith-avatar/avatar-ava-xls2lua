15d8fe0a527edb95ed79ec858e4ffd7972e3dacb
Yuancheng Zhang <zhangyuancheng@lilith.com>
2020-09-27 19:34:48 +0800

    add config gui w/o data

--- gui.py
+++ gui.py
@@ -19,43 +19,138 @@ __status__ = "Development"
 class MainFrame(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, -1, title, size=wx.Size(640, 320))
-
         self.SetIcon(wx.Icon("icon.ico"))
-
         self.panel = wx.Panel(self)
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer_v = wx.BoxSizer(wx.VERTICAL)
+
         # 采用多行显示
         self.logs = rt.RichTextCtrl(
             self.panel, style=wx.TE_MULTILINE | wx.TE_READONLY)
 
         # 1为响应容器改变大小，expand占据窗口的整个宽度
-        sizer.Add(self.logs, 1, wx.ALIGN_TOP | wx.EXPAND)
-        self.button = wx.Button(self.panel, label='Convert')
-        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
-        sizer.Add(self.button)
-        self.panel.SetSizerAndFit(sizer)
+        self.sizer_v.Add(self.logs, 1, wx.ALIGN_TOP | wx.EXPAND)
+        self.panel.SetSizerAndFit(self.sizer_v)
+
+        # Config
+        self.sizer_cfg_1 = wx.BoxSizer(wx.HORIZONTAL)
+        self.st1 = wx.StaticText(
+            self.panel, label=' Input Path:', size=(120, -1))
+        self.sizer_cfg_1.Add(self.st1, flag=wx.RIGHT, border=4)
+        self.tc1 = wx.TextCtrl(self.panel)
+        self.sizer_cfg_1.Add(self.tc1, proportion=1)
+
+        self.sizer_cfg_2 = wx.BoxSizer(wx.HORIZONTAL)
+        self.st2 = wx.StaticText(
+            self.panel, label=' Output Path:', size=(120, -1))
+        self.sizer_cfg_2.Add(self.st2, flag=wx.RIGHT, border=4)
+        self.tc2 = wx.TextCtrl(self.panel)
+        self.sizer_cfg_2.Add(self.tc2,  proportion=1)
+
+        self.sizer_cfg_3 = wx.BoxSizer(wx.HORIZONTAL)
+        self.st3 = wx.StaticText(
+            self.panel, label=' Output Lua Template:', size=(120, -1))
+        self.sizer_cfg_3.Add(self.st3, flag=wx.RIGHT, border=4)
+        self.tc3 = wx.TextCtrl(self.panel)
+        self.sizer_cfg_3.Add(self.tc3,  proportion=1)
+
+        self.sizer_v.Add(self.sizer_cfg_1, flag=wx.EXPAND |
+                         wx.LEFT | wx.RIGHT | wx.TOP, border=4)
+        self.sizer_v.Add(self.sizer_cfg_2, flag=wx.EXPAND |
+                         wx.LEFT | wx.RIGHT | wx.TOP, border=4)
+        self.sizer_v.Add(self.sizer_cfg_3, flag=wx.EXPAND |
+                         wx.LEFT | wx.RIGHT | wx.TOP, border=4)
+
+        # Bottom sizer
+        self.sizer_btm = wx.BoxSizer(wx.HORIZONTAL)
+
+        # Convert
+        self.btn_convert = wx.Button(self.panel, label='Convert')
+        self.Bind(wx.EVT_BUTTON, self.on_convert_click, self.btn_convert)
+        self.sizer_btm.Add(self.btn_convert)
+        self.sizer_btm.Add((30, -1))
+
+        # Config
+        self.cb_config = wx.CheckBox(self.panel, label='Config')
+        self.Bind(wx.EVT_CHECKBOX, self.on_config_checked, self.cb_config)
+        self.sizer_btm.Add(self.cb_config,
+                           flag=wx.RIGHT, border=4)
+
+        # Save
+        self.btn_save = wx.Button(self.panel, label='Save')
+        self.Bind(wx.EVT_BUTTON, self.on_save_click, self.btn_save)
+        self.sizer_btm.Add(self.btn_save,
+                           flag=wx.RIGHT, border=4)
+
+        self.sizer_v.Add(self.sizer_btm,  flag=wx.EXPAND |
+                         wx.LEFT | wx.RIGHT | wx.TOP, border=4)
+        self.sizer_v.Add((-1, 4))
+
+        # Init panel
         self.panel.Layout()
+        self.toggle_config()
 
     # 响应button事件
-    def OnClick(self, event):
+    def on_convert_click(self, event):
+        self.hide_config()
         if x2l is not None:
-            self.button.Disable()
+            self.btn_convert.Disable()
             self.logs.Clear()
             self.logs.Refresh()
             x2l.run()
-            self.button.Enable()
+            self.btn_convert.Enable()
         else:
             self.logs.WriteText('x2l is not found.')
+
+    def on_save_click(self, event):
+        self.save_config()
+
+    def save_config(self):
+        # todo
+        print('save config')
+        pass
+
+    def on_config_checked(self, event):
+        self.toggle_config()
+
+    def toggle_config(self):
+        if self.cb_config.IsChecked():
+            self.show_config()
+        else:
+            self.hide_config()
+
+    def show_config(self):
+        self.sizer_v.Show(self.sizer_cfg_1, recursive=True)
+        self.sizer_v.Show(self.sizer_cfg_2, recursive=True)
+        self.sizer_v.Show(self.sizer_cfg_3, recursive=True)
+        self.btn_save.Show()
+        self.panel.Layout()
+
+    def hide_config(self):
+        self.sizer_v.Hide(self.sizer_cfg_1, recursive=True)
+        self.sizer_v.Hide(self.sizer_cfg_2, recursive=True)
+        self.sizer_v.Hide(self.sizer_cfg_3, recursive=True)
+        self.btn_save.Hide()
+        self.panel.Layout()
 
     def write(self, s):
         self.logs.WriteText(s + '\n')
 
 
-if __name__ == '__main__':
-    app = wx.App()
-    mainFrame = MainFrame(None, 'Project DaVinci - XLS to LUA Convertor')
-    mainFrame.Show()
-    if x2l is not None:
-        x2l.set_gui(mainFrame)
+class App(wx.App):
+    def OnInit(self):
+        self.main_frame = MainFrame(
+            None, 'Project DaVinci - XLS to LUA Convertor')
+        self.main_frame.Show()
+        if x2l is not None:
+            x2l.set_gui(self.main_frame)
+        return True
+
+
+def main():
+    app = App()
     app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
