735f7db656c17afd8088fbd0a77637c0e1654136
Yuancheng Zhang <zhangyuancheng@lilith.com>
2020-09-29 14:41:59 +0800

    use .spec to build

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 :: Copyright 2020, Lilith Games, Project DaVinci, Avatar Team
 :: Author: Yuancheng Zhang
 :: git repo: https://github.com/lilith-avatar/avatar-ava-xls2lua
-powershell -command "pyinstaller .\gui.py -F -w -n ava-x2l -i icon.ico --clean --hidden-import wx --hidden-import wx._xml;cp .\dist\ava-x2l.exe .\ava-x2l.exe"
+powershell -command "pyinstaller .\ava-x2l.spec;cp .\dist\ava-x2l.exe .\ava-x2l.exe"
