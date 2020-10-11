aa51dacc94ba46431e242f963b742439c7ffd078
Yuancheng Zhang <zhangyuancheng@lilith.com>
2020-10-11 19:48:48 +0800

    python convert keys

--- ava_xls2lua.py
+++ ava_xls2lua.py
@@ -73,54 +73,50 @@ def make_table(filename):
             continue
         sheet_name = sheet_name[7:]
         # log(sheet_name +' sheet')
-        excel['data'][sheet_name] = {}
-        excel['meta'][sheet_name] = {}
+        data = excel['data'][sheet_name] = {}
+        meta = excel['meta'][sheet_name] = {}
 
         # 必须大于4行
         if sheet.nrows < 4:
             return {}, -1, 'sheet[{}] rows must > 4'.format(sheet_name)
 
-        # 解析标题
-        title = {}
-        row_idx, col_idx = 1, 0
-        for col_idx in range(sheet.ncols):
-            value = sheet.cell_value(row_idx, col_idx)
-            vtype = sheet.cell_type(row_idx, col_idx)
-            if vtype != xlrd.XL_CELL_TEXT:
-                return {}, -1, 'title columns[' + str(col_idx) + '] must be string'
-            title[col_idx] = str(value).replace(' ', '_')
-
-        excel['meta'][sheet_name]['title'] = title
-
-        # 类型解析
+        # 解析标题和类型
+        col_idx = 1
         type_dict = {}
-        row_idx, col_idx = 2, 0
         for col_idx in range(sheet.ncols):
-            value = sheet.cell_value(row_idx, col_idx)
-            vtype = sheet.cell_type(row_idx, col_idx)
-            type_dict[col_idx] = str(value)
-            if (type_dict[col_idx].lower() != INT
-                    and type_dict[col_idx].lower() != FLOAT
-                    and type_dict[col_idx].lower() != STRING
-                    and type_dict[col_idx].lower() != BOOL
-                    and type_dict[col_idx].lower() != INT_ARR
-                    and type_dict[col_idx].lower() != FLOAT_ARR
-                    and type_dict[col_idx].lower() != STRING_ARR
-                    and type_dict[col_idx].lower() != BOOL_ARR
-                    and type_dict[col_idx].lower() != VECTOR2
-                    and type_dict[col_idx].lower() != VECTOR3
-                    and type_dict[col_idx].lower() != EULER
-                    and type_dict[col_idx].lower() != COLOR
-                    and type_dict[col_idx].lower() != COMMENT
-                    and type_dict[col_idx].lower() != LUA):
-                return {}, -1, 'sheet[{}] row[{}] column[{}] type wrong'.format(sheet_name, row_idx, col_idx)
+            title = str(sheet.cell_value(1, col_idx)).replace(' ', '_')
+            title_type = sheet.cell_type(1, col_idx)
+            type_name = str(sheet.cell_value(2, col_idx)).lower()
+            type_type = sheet.cell_type(2, col_idx)
+            # 检查标题数据格式
+            if not title:
+                return {}, -1, 'sheet[{}] title columns[{}] must be string'.format(sheet_name, col_idx + 1)
+            if title_type != xlrd.XL_CELL_TEXT:
+                return {}, -1, 'sheet[{}] title columns[{}] must be string'.format(sheet_name, col_idx + 1)
+            # 检查类型数据格式
+            if type_type != xlrd.XL_CELL_TEXT:
+                return {}, -1, 'sheet[{}] type columns[{}] must be string'.format(sheet_name, col_idx + 1)
+            if (type_name != INT
+                        and type_name != FLOAT
+                        and type_name != STRING
+                        and type_name != BOOL
+                        and type_name != INT_ARR
+                        and type_name != FLOAT_ARR
+                        and type_name != STRING_ARR
+                        and type_name != BOOL_ARR
+                        and type_name != VECTOR2
+                        and type_name != VECTOR3
+                        and type_name != EULER
+                        and type_name != COLOR
+                        and type_name != COMMENT
+                        and type_name != LUA
+                    ):
+                return {}, -1, 'sheet[{}] type column[{}] type wrong'.format(sheet_name, col_idx + 1)
+            type_dict[title] = type_name
 
-        if type_dict[0].lower() != INT:
-            return {}, -1, 'sheet[{}] first column type must be [Int]'.format(sheet_name)
+        meta['type_dict'] = type_dict
 
-        excel['meta'][sheet_name]['type'] = type_dict
-
-        # * 读取主键key1，key2，key3，主键类型必须是Int或者String
+        # *读取主键key1，key2，key3，主键类型必须是Int或者String
         row_idx, col_idx = 3, 0
         for col_idx in range(sheet.ncols):
             key = sheet.cell_value(row_idx, col_idx).lower()
@@ -129,54 +125,93 @@ def make_table(filename):
             if key in (KEY_1, KEY_2, KEY_3):
                 if col_type not in (INT, STRING):
                     return {}, -1, 'sheet[{}] {} type must be Int or Float'.format(sheet_name, key)
-                excel[key] = col_name
+                meta[key] = col_name
 
         # 检查主键
-        if (KEY_3 in excel and not (KEY_2 in excel and KEY_1 in excel)) or (KEY_2 in excel and KEY_1 not in excel):
+        if (KEY_3 in meta and not (KEY_2 in meta and KEY_1 in meta)) or (KEY_2 in meta and KEY_1 not in meta):
             return {}, -1, 'sheet[{}] {} {} {} are wrong'.format(sheet_name, KEY_1, KEY_2, KEY_3)
+
+        key1 = meta[KEY_1] if KEY_1 in meta else None
+        key2 = meta[KEY_2] if KEY_2 in meta else None
+        key3 = meta[KEY_3] if KEY_3 in meta else None
 
         # 读取数据，从第5行开始
         row_idx = 4
         for row_idx in range(row_idx, sheet.nrows):
             row = {}
-
-            col_idx = 0
+            key_v1, key_v3, key_v2 = None, None, None
             for col_idx in range(sheet.ncols):
+                title = sheet.cell_value(1, col_idx)
                 value = sheet.cell_value(row_idx, col_idx)
                 vtype = sheet.cell_type(row_idx, col_idx)
                 # 本行有数据
                 v = None
-                if type_dict[col_idx].lower() == INT and vtype == xlrd.XL_CELL_NUMBER:
+                if type_dict[title] == INT and vtype == xlrd.XL_CELL_NUMBER:
                     v = int(value)
-                elif type_dict[col_idx].lower() == FLOAT and vtype == xlrd.XL_CELL_NUMBER:
+                elif type_dict[title] == FLOAT and vtype == xlrd.XL_CELL_NUMBER:
                     v = float(value)
-                elif type_dict[col_idx].lower() == STRING:
+                elif type_dict[title] == STRING:
                     v = format_str(value)
-                elif type_dict[col_idx].lower() == BOOL and vtype == xlrd.XL_CELL_BOOLEAN:
+                elif type_dict[title] == BOOL and vtype == xlrd.XL_CELL_BOOLEAN:
                     v = 'true' if value == 1 else 'false'
-                elif type_dict[col_idx].lower() == INT_ARR and vtype == xlrd.XL_CELL_TEXT:
+                elif type_dict[title] == INT_ARR and vtype == xlrd.XL_CELL_TEXT:
                     v = str(value)
-                elif type_dict[col_idx].lower() == FLOAT_ARR and vtype == xlrd.XL_CELL_TEXT:
+                elif type_dict[title] == FLOAT_ARR and vtype == xlrd.XL_CELL_TEXT:
                     v = str(value)
-                elif type_dict[col_idx].lower() == STRING_ARR:
+                elif type_dict[title] == STRING_ARR:
                     v = format_str(value)
-                elif type_dict[col_idx].lower() == BOOL_ARR and vtype == xlrd.XL_CELL_TEXT:
+                elif type_dict[title] == BOOL_ARR and vtype == xlrd.XL_CELL_TEXT:
                     v = str(value)
-                elif type_dict[col_idx].lower() == VECTOR2 and vtype == xlrd.XL_CELL_TEXT:
+                elif type_dict[title] == VECTOR2 and vtype == xlrd.XL_CELL_TEXT:
                     v = str(value)
-                elif type_dict[col_idx].lower() == VECTOR3 and vtype == xlrd.XL_CELL_TEXT:
+                elif type_dict[title] == VECTOR3 and vtype == xlrd.XL_CELL_TEXT:
                     v = str(value)
-                elif type_dict[col_idx].lower() == EULER and vtype == xlrd.XL_CELL_TEXT:
+                elif type_dict[title] == EULER and vtype == xlrd.XL_CELL_TEXT:
                     v = str(value)
-                elif type_dict[col_idx].lower() == COLOR and vtype == xlrd.XL_CELL_TEXT:
+                elif type_dict[title] == COLOR and vtype == xlrd.XL_CELL_TEXT:
                     v = str(value)
-                elif type_dict[col_idx].lower() == LUA and vtype == xlrd.XL_CELL_TEXT:
+                elif type_dict[title] == LUA and vtype == xlrd.XL_CELL_TEXT:
                     v = str(value)
-                elif type_dict[col_idx].lower() == COMMENT:
+                elif type_dict[title] == COMMENT:
                     continue
-                row[col_idx] = v
 
-            excel['data'][sheet_name][row[0]] = row
+                row[title] = v
+
+                if title == key1:
+                    key_v1 = v
+                if title == key2:
+                    key_v2 = v
+                if title == key3:
+                    key_v3 = v
+
+            # 键值检查
+            if key1 and key2 and key3:
+                if key_v1 not in data:
+                    data[key_v1] = {}
+                if key_v2 not in data[key_v1]:
+                    data[key_v1][key_v2] = {}
+                if not key_v3:
+                    return {}, -1, 'sheet[{}][{}] {} data "{}" is empty'.format(sheet_name, row_idx + 1, KEY_3, key3)
+                elif key_v3 in data[key_v1][key_v2]:
+                    return {}, -1, 'sheet[{}][{}] {} data "{}" is duplicated'.format(sheet_name, row_idx + 1, KEY_3, key3)
+                else:
+                    data[key_v1][key_v2][key_v3] = row
+            elif key1 and key2:
+                if key_v1 not in data:
+                    data[key_v1] = {}
+                if not key_v2:
+                    return {}, -1, 'sheet[{}][{}] {} data "{}" is empty'.format(sheet_name, row_idx + 1, KEY_2, key2)
+                elif key_v2 in data[key_v1]:
+                    return {}, -1, 'sheet[{}][{}] {} data "{}" is duplicated'.format(sheet_name, row_idx + 1, KEY_2, key2)
+                else:
+                    data[key_v1][key_v2] = row
+            elif key1:
+                if not key_v1:
+                    return {}, -1, 'sheet[{}][{}] {} data "{}" is empty'.format(sheet_name, row_idx + 1, KEY_1, key1)
+                elif key_v1 in data:
+                    return {}, -1, 'sheet[{}][{}] {} data "{}" is duplicated'.format(sheet_name, row_idx + 1, KEY_1, key1)
+                else:
+                    data[key_v1] = row
 
     return excel, 0, 'ok'
 
@@ -361,75 +396,77 @@ def get_lua(v):
 
 
 def write_to_lua_script(excel, output_path, xls_file):
+    return
     for (sheet_name, sheet) in excel['data'].items():
         file_name = OUTPUT_LUA_TEMPLATE.format(sheet_name=sheet_name)
         suffix = 'Xls'
         outfp = codecs.open(output_path + '/' + file_name, 'w', "utf-8")
         outfp.write(SCRIPT_HEAD % (excel['filename']))
         outfp.write('local ' + sheet_name + suffix + ' = {\r\n')
-        title = excel['meta'][sheet_name]['title']
-        type_dict = excel['meta'][sheet_name]['type']
+        type_dict = excel['meta'][sheet_name]['type_dict']
+        cnt = 0
         for (row_idx, row) in sheet.items():
             outfp.write('    [' + str(row_idx) + '] = {\r\n')
-            for (col_idx, field) in row.items():
-                if type_dict[col_idx].lower() == INT:
-                    tmp_str = get_int(row[col_idx])
+            for (key, value) in row.items():
+                if type_dict[key] == INT:
+                    tmp_str = get_int(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == FLOAT:
-                    tmp_str = get_float(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == FLOAT:
+                    tmp_str = get_float(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == STRING:
-                    tmp_str = get_string(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == STRING:
+                    tmp_str = get_string(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = \'' + str(tmp_str) + '\'')
-                elif type_dict[col_idx].lower() == BOOL:
-                    tmp_str = get_bool(row[col_idx])
+                        '        ' + key + ' = \'' + str(tmp_str) + '\'')
+                elif type_dict[key] == BOOL:
+                    tmp_str = get_bool(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == INT_ARR:
-                    tmp_str = get_int_arr(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == INT_ARR:
+                    tmp_str = get_int_arr(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == FLOAT_ARR:
-                    tmp_str = get_float_arr(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == FLOAT_ARR:
+                    tmp_str = get_float_arr(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == STRING_ARR:
-                    tmp_str = get_string_arr(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == STRING_ARR:
+                    tmp_str = get_string_arr(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == BOOL_ARR:
-                    tmp_str = get_bool_arr(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == BOOL_ARR:
+                    tmp_str = get_bool_arr(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == VECTOR2:
-                    tmp_str = get_vector2(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == VECTOR2:
+                    tmp_str = get_vector2(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == VECTOR3:
-                    tmp_str = get_vector3(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == VECTOR3:
+                    tmp_str = get_vector3(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == EULER:
-                    tmp_str = get_euler(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == EULER:
+                    tmp_str = get_euler(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == COLOR:
-                    tmp_str = get_color(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == COLOR:
+                    tmp_str = get_color(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
-                elif type_dict[col_idx].lower() == LUA:
-                    tmp_str = get_lua(row[col_idx])
+                        '        ' + key + ' = ' + str(tmp_str))
+                elif type_dict[key] == LUA:
+                    tmp_str = get_lua(value)
                     outfp.write(
-                        '        ' + str(title[col_idx]) + ' = ' + str(tmp_str))
+                        '        ' + key + ' = ' + str(tmp_str))
                 else:
                     outfp.close()
                     raise RuntimeError(
-                        'type "{}" is wrong.'.format(type_dict[col_idx]))
+                        'key "{}" type "{}" is wrong.'.format(key, type_dict[key]))
 
-                if col_idx == len(row.items()) - 1:
+                cnt += 1
+                if cnt == len(row):
                     outfp.write('\r\n')
                 else:
                     outfp.write(',\r\n')
@@ -512,8 +549,7 @@ def main():
 
     # filer files by .xls
     xls_files = [x for x in xls_files
-                 if (x[-4:] in ['.xls'] or x[-5:] in ['.xlsm', '.xlsx'])
-                 and x[0:2] not in ['~$']]
+                 if (x[-4:] in ['.xls'] or x[-5:] in ['.xlsm', '.xlsx']) and x[0:2] not in ['~$']]
     log(INFO + 'total XLS: \t\t{}'.format(len(xls_files)))
     # print(xls_files)
 
@@ -524,7 +560,6 @@ def main():
             log(FAILED + '[{}] {}'.format(i + 1, xls_file))
             raise RuntimeError(errstr)
         else:
-            # TODO 改成lua名字
             write_to_lua_script(t, output_path, xls_file)
 
 
