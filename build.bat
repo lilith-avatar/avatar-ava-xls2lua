7389d69ae82b0200c44a4b2727e395f018adac9b
Yuancheng Zhang <zhangyuancheng@lilith.com>
2020-10-23 14:53:38 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 :: Copyright 2020, Lilith Games, Project DaVinci, Avatar Team
 :: Author: Yuancheng Zhang
 :: git repo: https://github.com/lilith-avatar/avatar-ava-xls2lua
-powershell -command "pyinstaller .\ava-x2l.spec;cp .\dist\ava-x2l.exe .\ava-x2l.exe"
+powershell -command "python .\version_update.py .\gui.py .\ava_xls2lua.py;pyinstaller .\ava-x2l.spec;cp .\dist\ava-x2l.exe .\ava-x2l.exe"
