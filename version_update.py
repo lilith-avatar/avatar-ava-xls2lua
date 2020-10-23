5df1817340c4e83bc8da6d20684e69d8ca746cd7
Yuancheng Zhang <zhangyuancheng@lilith.com>
2020-10-23 14:47:35 +0800

    update version_update.py to v1.0

--- version_update.py
+++ version_update.py
@@ -3,9 +3,6 @@
 # description: Update other python file version
 # git repo: https://github.com/lilith-avatar/avatar-ava-xls2lua
 
-from tempfile import mkstemp
-from shutil import move, copymode
-from os import fdopen, remove
 import codecs
 import os
 import sys
@@ -14,24 +11,26 @@ __authors__ = ['Yuancheng Zhang']
 __copyright__ = 'Copyright 2020, Lilith Games, Project DaVinci, Avatar Team'
 __credits__ = ['Yuancheng Zhang']
 __license__ = 'MIT'
-__version__ = '0.1'
+__version__ = '1.0'
 __maintainer__ = 'Yuancheng Zhang'
-__status__ = 'Development'
+__status__ = 'Production'
 
 
 def update_version(file_path):
-    with codecs.open(file_path, 'r', 'utf-8') as fin:
+    tmp_file_path = file_path + '.tmp'
+    with codecs.open(file_path, 'r', 'utf-8') as fin, codecs.open(tmp_file_path, 'w', 'utf-8') as fout:
         lines = fin.readlines()
-        with codecs.open(file_path + '.tmp', 'w', 'utf-8') as fout:
-            for line in lines:
-                if line.startswith('__version__'):
-                    delim = '"' if '"' in line else "'"
-                    old_version = line.split(delim)[1]
-                    new_version = get_git_version()
-                    print('%s \t%s => %s' %
-                          (file_path.ljust(20), old_version, new_version))
-                    line = "__version = '%s'\n" % (new_version)
-                fout.write(line)
+        for line in lines:
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                old_version = line.split(delim)[1]
+                new_version = get_git_version()
+                print('%s \t%s => %s' %
+                      (file_path.ljust(20), old_version, new_version))
+                line = "__version__ = '%s'\n" % (new_version)
+            fout.write(line)
+    os.remove(file_path)
+    os.rename(tmp_file_path, file_path)
 
 
 def get_git_version():
