01eaba0cf11a4db6f515cdef4d45b5ed0259d057
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-07-06 16:52:13 +0800

    清空项目

--- build.bat
+++
@@ -1,5 +0,0 @@
-:: Build ava-xls2lua with PyInstaller (Windows)
-:: Copyright 2020, Lilith Games, Project DaVinci, Avatar Team
-:: Author: Yuancheng Zhang
-:: git repo: https://github.com/lilith-avatar/avatar-ava-xls2lua
-powershell -command "python .\version_update.py .\gui.py .\ava_xls2lua.py;pyinstaller .\ava-x2l.spec;cp .\dist\ava-x2l.exe .\ava-x2l.exe"
